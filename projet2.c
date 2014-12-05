6f007284c0ed15f36d3137f1de31332e3d54f51b
patou92700 <patrickmorcos@yopmail.com>
2014-12-05 11:10:47 +0100

    PSG + Projet2.c

---
+++ projet2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main()
+{
+    
+    
+    
+    
+return 0;
+}
+
+
+int TAILLE_CH = 256;
+int NB_CASES = 64;
+int NB_CASES_PAR_LIGNE = 8;
+
+int Damier[NB_CASES];
+string nomJ1;
+string nomJ2;
+int premierJoueurJoue;
+
